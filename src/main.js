8e07a6d3398d8463c720f971709b47b200ed02c6
yjjtt <yjjtt@tempot.com>
2021-05-25 08:58:21 +0800

    test directive

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+
+app.directive('pin', {
+  mounted(el, binding) {
+    el.style.position = 'fixed'
+    el.style.top = binding.value + 'px'
+  },
+})
+
+app.use(store).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p v-pin="100">辽宁达因</p>
   </div>
 </template>
 
